94dcadfeda928a27fd1bd6355b46fef3da534df0
Christian Gamperl <me@gibts.net>
2022-11-22 11:18:03 +0100

    Implement CalculatorController and tests

---
+++ src/main/java/at/tectrain/cicd/demo/CalculatorController.java
@@ -0,0 +1,40 @@
+package at.tectrain.cicd.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/calculator")
+public class CalculatorController {
+
+    private final CalculatorService service;
+
+    public CalculatorController(CalculatorService service) {
+        this.service = service;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/add")
+    public int add(@RequestParam int a, @RequestParam int b) {
+        return service.add(a, b);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/subtract")
+    public int subtract(@RequestParam int a, @RequestParam int b) {
+        return service.subtract(a, b);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/multiply")
+    public int multiply(@RequestParam int a, @RequestParam int b) {
+        return service.multiply(a, b);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/divide")
+    public int divide(@RequestParam int a, @RequestParam int b) {
+        return service.divide(a, b);
+    }
+
+}
